66a9ec405af75653549a7282fe5f7388b3d104b5
chewzzz <chewziqing@gmail.com>
2022-12-12 15:50:26 +0800

    Update projects data

--- projects/src/data/projects.js
+++ projects/src/data/projects.js
@@ -66,7 +66,14 @@ const projectData = [
                 externalLink: 'https://www.figma.com/file/q7u1AzIBAAsm09CK6P8mgx/Trailer-Browsing-App?node-id=7%3A47&t=zCqP438nPFSGvimv-0',
                 demoLink: "https://www.figma.com/proto/q7u1AzIBAAsm09CK6P8mgx/Trailer-Browsing-App?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2&show-proto-sidebar=1"
             },
-
+            {
+                name: 'Online Courses Website',
+                imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/UX-2.png',
+                desc: 'Low fidelity prototype for online courses website using Figma. Interactive prototype in progress.',
+                externalSite: 'Figma',
+                externalLink: 'https://www.figma.com/file/q7u1AzIBAAsm09CK6P8mgx/Trailer-Browsing-App?node-id=7%3A47&t=zCqP438nPFSGvimv-0',
+                demoLink: "https://www.figma.com/proto/q7u1AzIBAAsm09CK6P8mgx/Trailer-Browsing-App?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2&show-proto-sidebar=1"
+            },
         ]
     },
     {

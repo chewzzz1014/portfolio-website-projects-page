43645869883bb5bbdd7b2a5a4ea60708651a24c9
chewzzz <chewziqing@gmail.com>
2022-12-24 23:25:33 +0800

    Add MOOC full stack course projects section

--- projects/src/data/projects.js
+++ projects/src/data/projects.js
@@ -304,5 +304,18 @@ const projectData = [
             },
         ]
     },
+    {
+        name: 'Full Stack open 2022 Course by University of Helsinki MOOC Center',
+        icons: [''],
+        project: [
+            {
+                name: 'Online Courses Website',
+                imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/UX-2.png',
+                desc: 'Low fidelity prototype for online courses website using Figma. Interactive prototype in progress.',
+                srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/expense-tracker',
+                pageLink: 'https://chewzzz-expense-tracker-react.netlify.app/'
+            },
+        ]
+    },
 ]
 export default projectData

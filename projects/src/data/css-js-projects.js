4af5c5c88aaf97385733bff0e8a0c642d2deb4d7
chewzzz <chewziqing@gmail.com>
2022-12-26 16:51:13 +0800

    Move css-javascript projects to frontend catgory

--- projects/src/data/css-js-projects.js
+++ projects/src/data/css-js-projects.js
@@ -44,25 +44,11 @@ export default {
             pageLink: 'https://chewzzz-tvshow-api.netlify.app/'
         },
         {
-            name: 'Museum of Candy',
-            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/js-6.png',
-            desc: 'A responsive aesthetic website. Created using HTML, CSS and Boostrap.',
-            srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/museum_of_candy',
-            pageLink: 'https://chewzzz-museum-of-candy.netlify.app/'
-        },
-        {
             name: 'Pokemon Icons',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/js-7.png',
             desc: 'Generate 100 pokemon icons using JS DOM and Boostrap.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/pokemon_icon',
             pageLink: 'https://chewzzz-pokemon-icon.netlify.app/'
-        },
-        {
-            name: 'Pricing Panel Clone',
-            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/boostrap-2.png',
-            desc: 'A responsive pricing panel clone. Created using HTML and CSS.',
-            srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/pricing_panel',
-            pageLink: 'https://chewzzz-pricing-panel.netlify.app/'
         },
         {
             name: 'Rock Paper Scissors',
--- projects/src/data/frontend-projects.js
+++ projects/src/data/frontend-projects.js
@@ -9,5 +9,19 @@ export default {
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/ninjafood-tailwind',
             pageLink: 'https://chewzzz-ninjafood-tailwind.netlify.app/'
         },
+        {
+            name: 'Museum of Candy',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/js-6.png',
+            desc: 'A responsive aesthetic website. Created using HTML, CSS and Boostrap.',
+            srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/museum_of_candy',
+            pageLink: 'https://chewzzz-museum-of-candy.netlify.app/'
+        },
+        {
+            name: 'Pricing Panel Clone',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/boostrap-2.png',
+            desc: 'A responsive pricing panel clone. Created using HTML and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/pricing_panel',
+            pageLink: 'https://chewzzz-pricing-panel.netlify.app/'
+        },
     ]
 }

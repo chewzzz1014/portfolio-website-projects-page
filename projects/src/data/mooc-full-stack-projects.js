9d23d9865b6650f8fe9864b5d7d29dcd238dbc9f
chewzzz <chewziqing@gmail.com>
2022-12-26 16:53:38 +0800

    Remove mooc fi projects info

--- projects/src/data/mooc-full-stack-projects.js
+++ projects/src/data/mooc-full-stack-projects.js
@@ -3,11 +3,11 @@ export default {
     icons: [''],
     project: [
         {
-            name: 'Online Courses Website',
-            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/UX-2.png',
-            desc: 'Low fidelity prototype for online courses website using Figma. Interactive prototype in progress.',
-            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/expense-tracker',
-            pageLink: 'https://chewzzz-expense-tracker-react.netlify.app/'
+            name: 'Project Title',
+            imgLink: '',
+            desc: 'Project Description',
+            srcLink: '',
+            pageLink: ''
         },
     ]
 }

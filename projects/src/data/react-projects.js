ba6c9fc98ec8c0e728bdb7ec496266392a8ff998
chewzzz <chewziqing@gmail.com>
2022-12-26 16:40:24 +0800

    Move React projects into seperate file

---
+++ projects/src/data/react-projects.js
@@ -0,0 +1,69 @@
+export default {
+    name: 'React',
+    icons: [''],
+    project: [
+        {
+            name: 'Airbnb Clone',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-1.png',
+            desc: 'Responsive airbnb clone. Created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/airbnb-clone',
+            pageLink: 'https://chewzzz-airbnb-clone.netlify.app/',
+        },
+        {
+            name: 'Digital Business Card',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-2.png',
+            desc: 'Responsive personal business card. Created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/digital-business-card',
+            pageLink: 'https://chewzzz-digital-business-card.netlify.app/',
+        },
+        {
+            name: 'Meme Generator',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-3.png',
+            desc: 'A fun meme generator with customised caption. Created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/meme-generator',
+            pageLink: 'https://chewzzz-meme-generator.netlify.app/',
+        },
+        {
+            name: 'Random Quote Machine',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-4.png',
+            desc: 'Generate random positive quote with random background colour. Created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/random-quote-machine',
+            pageLink: 'https://chewzzz-quote-machine.netlify.app/',
+        },
+        {
+            name: 'React Fact',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-5.png',
+            desc: 'My first React project. A static React fact website with toggleable dark theme and light theme. Created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/React-Fact',
+            pageLink: 'https://chewzzz-react-fact.netlify.app/',
+        },
+        {
+            name: 'React Form',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-6.png',
+            desc: 'Simple react form that displays user inputs. Created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/react-form',
+            pageLink: 'https://chewzzz-react-form.netlify.app/',
+        },
+        {
+            name: 'Rolling Dice',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-7.png',
+            desc: 'A simple dices rolling game. Rolling dices until all dices are of same digit. Created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/tenzies-game',
+            pageLink: 'https://chewzzz-rolling-dice.netlify.app/',
+        },
+        {
+            name: 'Travel Journal',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-8.png',
+            desc: 'Responsive travel location website created with React and CSS.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/travel-journal',
+            pageLink: 'https://chewzzz-travel-journal.netlify.app/',
+        },
+        {
+            name: 'Expense Tracker',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/react-9.png',
+            desc: 'Simple expense tracker app created with React and CSS. User can add and delete transaction history items.',
+            srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/expense-tracker',
+            pageLink: 'https://chewzzz-expense-tracker-react.netlify.app/',
+        },
+    ]
+}

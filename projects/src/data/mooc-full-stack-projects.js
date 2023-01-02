f785569f10ffbbc1326347a625b29c3eeff8f65b
chewzzz <chewziqing@gmail.com>
2023-01-02 21:42:09 +0800

    Update image url

--- projects/src/data/mooc-full-stack-projects.js
+++ projects/src/data/mooc-full-stack-projects.js
@@ -4,7 +4,7 @@ export default {
     project: [
         {
             name: 'Anecdotes',
-            imgLink: '',
+            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/mooc-1.png',
             desc: 'Part 1 Introduction to React exercises. A simple React program that displays anecdotes and their votes.',
             srcLink: 'https://github.com/chewzzz1014/MoocFi-Full-Stack-Open-2022/tree/master/part1/anecdotes',
             pageLink: ''

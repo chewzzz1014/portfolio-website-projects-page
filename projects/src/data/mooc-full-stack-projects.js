4b892a44069b4579f63101c32f4cc9083578d532
chewzzz <chewziqing@gmail.com>
2023-01-14 16:23:24 +0800

    Hide project

--- projects/src/data/mooc-full-stack-projects.js
+++ projects/src/data/mooc-full-stack-projects.js
@@ -16,12 +16,12 @@ export default {
             srcLink: 'https://github.com/chewzzz1014/MoocFi-Full-Stack-Open-2022/tree/master/part1/unicafe',
             pageLink: 'https://chewzzz-mooc-unicafe.netlify.app/'
         },
-        {
-            name: 'Countries Info',
-            imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/mooc-3.png',
-            desc: "Part 2 Communicating with server exercises. A countries information page that enables user to search, filter countries and get countries information, especially weather info that fetched from API. (Error: Cant read env variable from .env. I'll change it later",
-            srcLink: 'https://github.com/chewzzz1014/MoocFi-Full-Stack-Open-2022/tree/master/part2/countries',
-            pageLink: 'https://chewzzz-mooc-countries.netlify.app/'
-        },
+        // {
+        //     name: 'Countries Info',
+        //     imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/mooc-3.png',
+        //     desc: "Part 2 Communicating with server exercises. A countries information page that enables user to search, filter countries and get countries information, especially weather info that fetched from API. (Error: Cant read env variable from .env. I'll change it later",
+        //     srcLink: 'https://github.com/chewzzz1014/MoocFi-Full-Stack-Open-2022/tree/master/part2/countries',
+        //     pageLink: 'https://chewzzz-mooc-countries.netlify.app/'
+        // },
     ]
 }

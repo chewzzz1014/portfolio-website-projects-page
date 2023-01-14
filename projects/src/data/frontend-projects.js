78634aa4524dc17bfd8b4b59acf0ea24c7113ea5
chewzzz <chewziqing@gmail.com>
2023-01-14 16:22:22 +0800

    Edit desc text

--- projects/src/data/frontend-projects.js
+++ projects/src/data/frontend-projects.js
@@ -19,7 +19,7 @@ export default {
         {
             name: 'Ninja Food',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/frontend-1.png',
-            desc: 'Resonsive recipe website created using HTML and Tailwind. My first project using Tailwind.',
+            desc: 'Resonsive recipe website created using HTML and Tailwind. My first Tailwind project.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/ninjafood-tailwind',
             pageLink: 'https://chewzzz-ninjafood-tailwind.netlify.app/'
         },

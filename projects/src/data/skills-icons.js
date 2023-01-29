48389e01a18700ee5099ba6c8ef8217d60f557f8
chewzzz <chewziqing@gmail.com>
2023-01-29 10:03:35 +0800

    Add skills dev icons

---
+++ projects/src/data/skills-icons.js
@@ -0,0 +1,72 @@
+const skills = [
+    {
+        name: 'Java',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg'
+    },
+    {
+        name: 'JavaScript',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+    },
+    {
+        name: 'Python',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
+    },
+    {
+        name: 'HTML',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+    },
+    {
+        name: 'CSS',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+    },
+    {
+        name: 'TypeScript',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
+    },
+    {
+        name: 'React',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+    },
+    {
+        name: 'Express',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg'
+    },
+    {
+        name: 'Node js',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg'
+    },
+    {
+        name: 'Mongodb',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
+    },
+    {
+        name: "Oracle SQL",
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg'
+    },
+    {
+        name: 'Git',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'
+    },
+    {
+        name: 'Boostrap',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg'
+    },
+    {
+        name: 'Tailwind CSS',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg'
+    },
+    {
+        name: 'Figma',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
+    },
+    {
+        name: 'Pandas',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg'
+    },
+    {
+        name: 'Markdown',
+        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg'
+    }
+]
+
+export default skills

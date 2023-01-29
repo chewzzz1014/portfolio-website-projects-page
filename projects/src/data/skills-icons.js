bf68fb4db7efec391fb7803bae247e9c19e741ee
chewzzz <chewziqing@gmail.com>
2023-01-29 10:09:45 +0800

    Convert array to object

--- projects/src/data/skills-icons.js
+++ projects/src/data/skills-icons.js
@@ -1,72 +1,38 @@
-const skills = [
-    {
-        name: 'Java',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg'
-    },
-    {
-        name: 'JavaScript',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
-    },
-    {
-        name: 'Python',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
-    },
-    {
-        name: 'HTML',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
-    },
-    {
-        name: 'CSS',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
-    },
-    {
-        name: 'TypeScript',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
-    },
-    {
-        name: 'React',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
-    },
-    {
-        name: 'Express',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg'
-    },
-    {
-        name: 'Node js',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg'
-    },
-    {
-        name: 'Mongodb',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
-    },
-    {
-        name: "Oracle SQL",
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg'
-    },
-    {
-        name: 'Git',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'
-    },
-    {
-        name: 'Boostrap',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg'
-    },
-    {
-        name: 'Tailwind CSS',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg'
-    },
-    {
-        name: 'Figma',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
-    },
-    {
-        name: 'Pandas',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg'
-    },
-    {
-        name: 'Markdown',
-        imgLink: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg'
-    }
-]
+const skills = {
+    'Java': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
+
+    'JavaScript': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+
+    'Python': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+
+    'HTML': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+
+    'CSS': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+
+    'TypeScript': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+
+    'React': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+
+    'Express': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+
+    'Node js': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg',
+
+    'Mongodb': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
+
+    "Oracle SQL": 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg',
+
+    'Git': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+
+    'Boostrap': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+
+    'Tailwind CSS': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+
+    'Figma': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
+
+    'Pandas': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg',
+
+    'Markdown': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg',
+
+}
 
 export default skills

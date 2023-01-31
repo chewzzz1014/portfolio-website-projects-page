d3e0195b281cf3a009db4d01699065e4a2c0eaa3
chewzzz <chewziqing@gmail.com>
2023-01-31 15:32:30 +0800

    Add skills icons for all frontend projects

--- projects/src/data/projects/frontend-projects.js
+++ projects/src/data/projects/frontend-projects.js
@@ -7,42 +7,48 @@ const projects = {
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/js-6.png',
             desc: 'A responsive aesthetic website. Created using HTML, CSS and Boostrap.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/museum_of_candy',
-            pageLink: 'https://chewzzz-museum-of-candy.netlify.app/'
+            pageLink: 'https://chewzzz-museum-of-candy.netlify.app/',
+            skills: ["html", "css", 'boostrap'],
         },
         {
             name: 'Pricing Panel Clone',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/boostrap-2.png',
             desc: 'A responsive pricing panel clone. Created using HTML and CSS.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/pricing_panel',
-            pageLink: 'https://chewzzz-pricing-panel.netlify.app/'
+            pageLink: 'https://chewzzz-pricing-panel.netlify.app/',
+            skills: ["html", "css"],
         },
         {
             name: 'Ninja Food',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/frontend-1.png',
             desc: 'Resonsive recipe website created using HTML and Tailwind. My first Tailwind project.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/ninjafood-tailwind',
-            pageLink: 'https://chewzzz-ninjafood-tailwind.netlify.app/'
+            pageLink: 'https://chewzzz-ninjafood-tailwind.netlify.app/',
+            skills: ["html", "tailwind"],
         },
         {
             name: 'Tailwind Card',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/frontend-2.png',
             desc: 'Responsive card item created using HTML and Tailwind.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/tailwind-card',
-            pageLink: 'https://chewzzz-tailwind-card.netlify.app/'
+            pageLink: 'https://chewzzz-tailwind-card.netlify.app/',
+            skills: ["html", "tailwind"],
         },
         {
             name: 'Responsive Form',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/frontend-3.png',
             desc: 'Simple, responsive form created using HTML and Tailwind.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/simple-form',
-            pageLink: 'https://chewzzz-simple-form.netlify.app/'
+            pageLink: 'https://chewzzz-simple-form.netlify.app/',
+            skills: ["html", "tailwind"],
         },
         {
-            name: 'Responsive Form',
+            name: 'Rockets Acme',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/frontend-4.png',
             desc: 'Responsive wesbite created using HTML and Tailwind. Learning CSS animation.',
             srcLink: 'https://github.com/chewzzz1014/css-javascript-projects/tree/master/tailwind-tutorial/rockets-acme',
-            pageLink: 'https://chewzzz-rockets-acme.netlify.app/'
+            pageLink: 'https://chewzzz-rockets-acme.netlify.app/',
+            skills: ["html", "tailwind"],
         },
     ]
 }

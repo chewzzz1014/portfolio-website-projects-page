46f8c890e922fbce4852c1aa6aec793d25fb07c7
chewzzz <chewziqing@gmail.com>
2023-01-31 15:38:39 +0800

    Add skills icons for all express projects

--- projects/src/data/projects/express-projects.js
+++ projects/src/data/projects/express-projects.js
@@ -8,6 +8,7 @@ const projects = {
             desc: 'Create users, add and filtering exercise logs. Made with Express and MongoDB.',
             srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Exercise-Tracker',
             pageLink: 'https://exercise-tracker-express.onrender.com/',
+            skills: ['express', 'mongodb'],
         },
         {
             name: 'File Metadata Microservice',
@@ -15,6 +16,7 @@ const projects = {
             desc: 'Provide information about the file uploaded.',
             srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/File-Metadata-Microservice',
             pageLink: 'https://chewzzz-file-metadata-microservice.onrender.com/',
+            skills: ['express', 'node'],
         },
         {
             name: 'Request Header Parser Microservice',
@@ -22,6 +24,7 @@ const projects = {
             desc: 'Obtain ip address, language and browser used by user who sent the request.',
             srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Request-Header-Parser-Microservice',
             pageLink: 'https://request-header-parser-microservice-mv5m.onrender.com/',
+            skills: ['express', 'node'],
         },
         {
             name: 'Timestamp Microservice',
@@ -29,6 +32,7 @@ const projects = {
             desc: 'Convert date to UNIX timestamp.',
             srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/Timestamp-Microservice',
             pageLink: 'https://timestamp-microservice-express.onrender.com/',
+            skills: ['express'],
         },
         {
             name: 'URL Shortener Microservice',
@@ -36,6 +40,7 @@ const projects = {
             desc: 'Convert URL to short url. Made Express and MongoDB.',
             srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/URL-Shortener-Microservice',
             pageLink: 'https://chewzzz-url-shortener-microservice.onrender.com',
+            skills: ['express', 'mongodb'],
         },
         {
             name: 'Node Passport Authentication',
@@ -43,6 +48,7 @@ const projects = {
             desc: 'Boilerplate for authentication and session in Express project.Made with Express, MongodDB and EJS.',
             srcLink: 'https://github.com/chewzzz1014/JS-Frameworks-Projects/tree/master/node-passport-authentication',
             pageLink: 'https://chewzzz-node-passport-authentication.onrender.com',
+            skills: ['express', 'mongodb', 'node', 'boostrap'],
         },
     ]
 }

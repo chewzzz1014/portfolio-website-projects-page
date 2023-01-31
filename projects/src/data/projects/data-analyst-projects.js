e7737a9e986320e91261b8d7328dc737ff9d7e87
chewzzz <chewziqing@gmail.com>
2023-01-31 15:41:56 +0800

    Add skills icons for all data anaylsy projects

--- projects/src/data/projects/data-analyst-projects.js
+++ projects/src/data/projects/data-analyst-projects.js
@@ -9,49 +9,56 @@ const projects = {
             srcLink: 'https://github.com/chewzzz1014/data-analysts-projects/blob/master/anime/anime.ipynb',
             pageLink: '',
             externalSite: 'Kaggle',
-            externalLink: 'https://www.kaggle.com/code/chewziqing/anime-data-set/notebook'
+            externalLink: 'https://www.kaggle.com/code/chewziqing/anime-data-set/notebook',
+            skills: ['python', 'pandas']
         },
         {
             name: 'Netflix Original Films and IMDB Scores',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/data-analyst-2.png',
             desc: 'Perform data cleaning, EDA and visualization on the movies IMDB Scores dataset from Kaggle using Pandas library. Course project for the course Data Analysis with Python: Zero to Pandas',
             srcLink: 'https://github.com/chewzzz1014/data-analysts-projects/blob/master/movies-imdb/project-imdb-score.ipynb',
-            pageLink: ''
+            pageLink: '',
+            skills: ['python', 'pandas']
         },
         {
             name: 'Cryptocurrencies Prices Visulization',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/data-analyst-3.png',
             desc: 'Visualize cryptocurrencies prices from public API and download them as Excel files using Pandas library.',
             srcLink: 'https://github.com/chewzzz1014/data-analysts-projects/blob/master/cryptocurrencies-prices/cryptocurrencies-prices.ipynb',
-            pageLink: ''
+            pageLink: '',
+            skills: ['python', 'pandas']
         },
         {
             name: 'Mean Variance Standard Deviation Calculator',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/data-analyst-4.png',
             desc: 'Calculate the mean, variance, standard deviation, max, min, and sum of the rows, columns, and elements in a 3 x 3 matrix. One of the graduation projects required for Data Analysis with Python course on Free Code Camp',
             srcLink: 'https://github.com/chewzzz1014/data-analysts-projects/tree/master/freeCodeCamp/Mean-Variance-Standard_Deviation_Calculator',
-            pageLink: ''
+            pageLink: '',
+            skills: ['python', 'pandas']
         },
         {
             name: 'Demographic Data Analyzer',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/data-analyst-5.png',
             desc: 'Extracting demographic information based on analysis on the given dataset using Pandas library. One of the graduation projects required for Data Analysis with Python course on Free Code Camp',
             srcLink: 'https://github.com/chewzzz1014/data-analysts-projects/tree/master/freeCodeCamp/Demographic-Data-Analyzer',
-            pageLink: ''
+            pageLink: '',
+            skills: ['python', 'pandas']
         },
         {
             name: 'Medical Data Visualizer',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/data-analyst-6.png',
             desc: 'Visualising and make calculations from medical examination data using Matplotlib, Seaborn, and Pandas library. One of the graduation projects required for Data Analysis with Python course on Free Code Camp',
             srcLink: 'https://github.com/chewzzz1014/data-analysts-projects/tree/master/freeCodeCamp/Medical-Data-Visualizer',
-            pageLink: ''
+            pageLink: '',
+            skills: ['python', 'pandas']
         },
         {
             name: 'Pages View Time Series Visualizer',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/data-analyst-7.png',
             desc: 'Visualize time series data of number of page views each day on the freeCodeCamp.org forum for a given period. One of the graduation projects required for Data Analysis with Python course on Free Code Camp',
             srcLink: 'https://github.com/chewzzz1014/data-analysts-projects/tree/master/freeCodeCamp/Page-View-Time-Series-Visualizer',
-            pageLink: ''
+            pageLink: '',
+            skills: ['python', 'pandas']
         },
     ]
 }

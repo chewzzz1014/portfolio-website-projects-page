4558547c8acdf9aed3eb464a1e2b7c957cfadfd8
chewzzz <chewziqing@gmail.com>
2023-01-31 15:46:07 +0800

    Add skills icons for ux design projects

--- projects/src/data/projects/mooc-full-stack-projects.js
+++ projects/src/data/projects/mooc-full-stack-projects.js
@@ -7,14 +7,16 @@ const projects = {
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/mooc-1.png',
             desc: 'Part 1 Introduction to React exercises. A simple React program that displays anecdotes and their votes.',
             srcLink: 'https://github.com/chewzzz1014/MoocFi-Full-Stack-Open-2022/tree/master/part1/anecdotes',
-            pageLink: 'https://chewzzz-mooc-anecdotes.netlify.app/'
+            pageLink: 'https://chewzzz-mooc-anecdotes.netlify.app/',
+            skills: ['react'],
         },
         {
             name: 'Unicafe',
             imgLink: 'https://cdn.jsdelivr.net/gh/chewzzz1014/portfolio-website-projects-page/projects/src/images/mooc-2.png',
             desc: 'Part 1 Introduction to React exercises. A simple React program that calculates feedback statictics.',
             srcLink: 'https://github.com/chewzzz1014/MoocFi-Full-Stack-Open-2022/tree/master/part1/unicafe',
-            pageLink: 'https://chewzzz-mooc-unicafe.netlify.app/'
+            pageLink: 'https://chewzzz-mooc-unicafe.netlify.app/',
+            skills: ['react'],
         },
         // {
         //     name: 'Countries Info',
